9fed66a573a8adf3f339769165a9583cdb83c704
ZidXCode <mohomedzd@gmail.com>
2023-11-11 02:19:20 +0530

    diceGame

---
+++ script.js
@@ -0,0 +1,54 @@
+let images=["1.png","2.png","3.png","4.png","5.png","6.png"];
+
+let dice= document.querySelectorAll("img");
+
+
+$(document).ready(function(){
+    $("#btn").click(function(){
+        $("#dice1").animate({
+            rotate:'12deg',
+        },400, function(){
+            //complete
+        });
+
+        $("#dice2").animate({
+            rotate:'12deg',
+        },400, function(){
+            //complete
+        });
+
+        $("#dice1").animate({
+            rotate:'-12deg',
+        },400, function(){
+            //complete
+        });
+
+        $("#dice2").animate({
+            rotate:'-12deg',
+        },400, function(){
+            //complete
+        });
+
+        $("#dice1").animate({
+            rotate:'0deg',
+        },400, function(){
+            //complete
+        });
+
+        $("#dice2").animate({
+            rotate:'0deg',
+        },400, function(){
+            
+            let diceNumber1= parseInt(Math.random()*6);
+            let diceNumber2= parseInt(Math.random()*6);
+
+            document.querySelector("#dice1").setAttribute("src",images[diceNumber1]);
+            document.querySelector("#dice2").setAttribute("src",images[diceNumber2]);
+
+            let sum= diceNumber1 + 1 +diceNumber2 + 1;
+
+            document.getElementById("sum").innerHTML=sum;
+        });
+
+    });
+});
