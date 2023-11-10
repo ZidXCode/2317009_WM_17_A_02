ca82ec28befbda67e0323c9546e66b6c455d39ae
ZidXCode <mohomedzd@gmail.com>
2023-11-11 02:41:33 +0530

    diceGame

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let images=["1.png","2.png","3.png","4.png","5.png","6.png"];
 
 let dice= document.querySelectorAll("img");
 
+//animation
 
 $(document).ready(function(){
     $("#btn").click(function(){
@@ -38,6 +39,8 @@ $(document).ready(function(){
         $("#dice2").animate({
             rotate:'0deg',
         },400, function(){
+
+            //calculations
             
             let diceNumber1= parseInt(Math.random()*6);
             let diceNumber2= parseInt(Math.random()*6);
